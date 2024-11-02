c43c219d572e04d8018f56cd3ed3b91b59a8efe4
michalLewenstein <lmichal2024@gmail.com>
2024-11-02 23:00:59 +0200

    Add files via upload

---
+++ eventWeb/Event.cs
@@ -0,0 +1,19 @@
+﻿namespace WebEvents
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+
+        public Event(int id, string title, DateTime start, DateTime end)
+        {
+            Id = id;
+            Title = title;
+            this.start = start;
+            this.end = end;
+        }
+        public Event() { }
+    }
+}
